3fb0c3b8485f66f799c6aea6a84f78eb11deee94
Kseniya <pupkoksenia@gmail.com>
2022-09-16 15:04:45 +0300

    remove ref

--- src/components/NotFoundPage.vue
+++ src/components/NotFoundPage.vue
@@ -16,7 +16,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent } from "vue";
 import { useFireBase } from "../composables/useFireBase";
 import { useRouter } from "vue-router";
 
